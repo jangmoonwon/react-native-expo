ca3a587396eaf12d1b93dfcab0f6b905c4fee480
MOON <muneon96@naver.com>
2020-07-27 22:25:29 +0900

    [TodoInput] icon 생성 및 수정

--- components/TodoListItem.js
+++ components/TodoListItem.js
@@ -1,42 +1,71 @@
-import React from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+
 
 const TodoListItem = () => {
-    return (
-      <View style={styles.listItemContainer}>
-        <TouchableOpacity>
-          <View style={styles.circleBtn} />
-        </TouchableOpacity>
-        <Text style={styles.todoText}>첫 번째 할 일</Text>
-      </View>
-    );
-  };
+  return (
+    <View style={styles.listItemContainer}>
+      <TouchableOpacity>
+        <View style={styles.completeCircle}>
+          <AntDesign name="checkcircle" size={27} color="black" />
+        </View>
+      </TouchableOpacity>
+      <Text style={[styles.text, styles.strikeText]}>첫 번째 할 일</Text>
+      <TouchableOpacity style={styles.buttonContainer}>
+    <Text style={styles.buttonText}>
+    <AntDesign name="minuscircleo" size={27} color="#ff7272" />
+    </Text>
+</TouchableOpacity>
+    </View>
+  );
+};
 
-  const styles = StyleSheet.create({
-    listItemContainer: {
-      flex: 1,
-      borderBottomColor: '#bbb',
-      borderBottomWidth: StyleSheet.hairlineWidth,
-      flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'space-between',
-    },
-    todoText: {
-      flex: 5,
-      fontWeight: '500',
-      fontSize: 18,
-      marginVertical: 20,
-      width: 100,
-    },
-    circleBtn: {
-      width: 30,
-      height: 30,
-      borderRadius: 15,
-      borderColor: '#ff7272',
-      borderWidth: 2,
-      marginRight: 20,
-      marginLeft: 20,
-    },
-  });
-  
-  export default TodoListItem;
+const styles = StyleSheet.create({
+  listItemContainer: {
+    flex: 1,
+    borderBottomColor: "#bbb",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  todoText: {
+    flex: 5,
+    fontWeight: "500",
+    fontSize: 18,
+    marginVertical: 20,
+    width: 100,
+  },
+  circleBtn: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    borderColor: "#ff7272",
+    borderWidth: 2,
+    marginRight: 20,
+    marginLeft: 20,
+  },
+  completeCircle: {
+    marginRight: 20,
+    marginLeft: 20,
+  },
+  strikeText: {
+    flex: 5,
+    fontWeight: "500",
+    fontSize: 18,
+    marginVertical: 20,
+    width: 100,
+    color: "#bbb",
+    textDecorationLine: "line-through",
+  },
+  unstrikeText: {
+    color: "#29323c",
+  },
+  buttonContainer: {
+    marginVertical: 10,
+    marginHorizontal: 10,
+  },
+});
+
+export default TodoListItem;
